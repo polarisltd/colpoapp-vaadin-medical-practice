97d9670808a834f7cfb4ab2d1a1def2a60b506d5
polarisltd <roberts.polis@sets.lv>
2024-07-08 09:27:41 +0300

    docker is working. Dataloader fix for prod.

--- README.md
+++ README.md
@@ -1,17 +1,41 @@
-# Spring Boot and Vaadin course source code
-
-This repository contains the source code for the [Building Modern Web Applications With Spring Boot and Vaadin](https://vaadin.com/docs/latest/flow/tutorials/in-depth-course).
-
-*Live demo:* https://crm.demo.vaadin.com
-
-## Running the Application
-There are two ways to run the application:  using `mvn` or by running the `Application` class directly from your IDE.
-
-## Branches
-
-- The main branch contains the source code for the latest Vaadin release
-- The `v14` branch contains the source code for Vaadin 14
+# Colposcopy Vaadin Application
 
 
-## Text tutorial
-You can find a text version of the tutorial in the [Vaadin Documentation](https://vaadin.com/docs/latest/flow/tutorials/in-depth-course).
+
+## production build
+Production build will package clientside resources.
+
+> mvn clean package -Pproduction
+
+## build image
+
+> docker build -f DockerfileApp -t colpoapp-vaadin .
+
+notes for dockerfile:
+
+please update dockerfile to include -Dspring.profiles.active=prod so that production properties got properly applied.
+```bash
+ENTRYPOINT java -jar -Dspring.profiles.active=prod /myapp/colpoapp-vaadin-1.0-SNAPSHOT.jar -d64 -Xmx8g -XX:+CrashOnOutOfMemoryError
+```
+please make sure that application image used in docker build is the same as in docker compose.
+
+
+```
+# build and run app
+mvn clean package -Pproduction -DskipTests 
+docker build -f DockerfileApp -t colpoapp-vaadin-4 .
+#run via docker. issue will be hostname postgresql not found. to make this work, you need to run via docker-compose.
+docker run -p 8080:8080 colpoapp-vaadin-4:latest
+# run via docker-compose
+docker-compose up -d
+```
+notes:
+
+- about hostnames in docker environment. 
+
+When you run docker-compose up, Docker Compose automatically creates a network for your app and adds all the services in your app to that network. Containers on the same network can reach each other using the service name as the hostname.
+
+issues:
+
+- when running with -Dspring.profiles.active=prod application-prod.properties is not being overlapped!.
+
--- src/main/java/com/example/application/data/DataLoader.java
+++ src/main/java/com/example/application/data/DataLoader.java
@@ -1,46 +1,69 @@
 package com.example.application.data;
 
 
+import com.example.application.pdf.PdfHelloWorld;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class DataLoader implements CommandLineRunner {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataLoader.class);
     private DakterisRepository drRepository;
     private PatientsRepository ptRepository;
     private ImageRepository imRepository;
     private KolposkopijaIzmeklejumsRepository visitRepository;
+    private Long drId;
+    private Long ptId;
+    private Long visitId;
+    Environment env;
+
     public DataLoader(DakterisRepository drRepository,
                       PatientsRepository ptRepository,
                       ImageRepository imRepository,
-                      KolposkopijaIzmeklejumsRepository visitRepository ){
+                      KolposkopijaIzmeklejumsRepository visitRepository,
+                      Environment env){
         this.drRepository = drRepository;
         this.ptRepository = ptRepository;
         this.imRepository = imRepository;
         this.visitRepository = visitRepository;
+        this.env = env;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        loadDrs();
-        loadPacients();
-        loadVisit();
-        loadImages();
+        // ignore for -Dspring.profiles.active=prod
+        boolean isProdActive = Arrays.asList(env.getActiveProfiles()).contains("prod");
+        if (!isProdActive) {
+            loadDrs();
+            loadPacients();
+            loadVisit();
+            loadImages();
+        }
     }
 
 
     private void loadDrs() {
         if(drRepository.count()==0) {
+          try{
             DakterisEntity dakterisEntity = DakterisEntity.builder()
                     .id(1L)
                     .vardsUzvardsDakteris("Dr. Kolposkops Inese")
                     .build();
             drRepository.save(dakterisEntity);
+            drId = dakterisEntity.getId();
+        }catch(Exception e) {
+            LOGGER.error("Error loading Visit: ", e);
+        }
         }
     }
     private void loadPacients() {
         if(ptRepository.count()==0) {
+         try{
             PacientsEntity pacientsEntity = PacientsEntity.builder()
                     .id(1L)
                     .vardsUzvardsPacients("Janis Berzins")
@@ -48,28 +71,41 @@ public class DataLoader implements CommandLineRunner {
                     .dzimsanasGads(1980)
                     .build();
             ptRepository.save(pacientsEntity);
+            ptId = pacientsEntity.getId();
+        }catch(Exception e) {
+            LOGGER.error("Error loading Patients: ", e);
+        }
         }
     }
 
     private void loadVisit() {
         if(visitRepository.count()==0) {
-            KolposkopijaIzmeklejumsEntity visitEntity = KolposkopijaIzmeklejumsEntity.builder()
-                    .id(1L)
-                    .dakteris(drRepository.findById(1L).get())
-                    .pacients(ptRepository.findById(1L).get())
-                    .build();
-            visitRepository.save(visitEntity);
+            try {
+                KolposkopijaIzmeklejumsEntity visitEntity = KolposkopijaIzmeklejumsEntity.builder()
+                        .id(1L)
+                        .dakteris(drRepository.findById(drId).get())
+                        .pacients(ptRepository.findById(ptId).get())
+                        .build();
+                visitRepository.save(visitEntity);
+                this.visitId = visitEntity.getId();
+            }catch(Exception e) {
+                LOGGER.error("Error loading Visit: ", e);
+            }
         }
     }
 
     private void loadImages() {
         if(imRepository.count()==0) {
+        try{
             ImageEntity imagesEntity = ImageEntity.builder()
                     .id(1)
-                    .visitId(1)
+                    .visitId(this.visitId.intValue())
                     .imagePath("C:\\Users\\polar\\Downloads\\001\\pog009-3.jpg")
                     .build();
             imRepository.save(imagesEntity);
+        }catch(Exception e) {
+            LOGGER.error("Error loading Image: ", e);
+        }
         }
     }
 
